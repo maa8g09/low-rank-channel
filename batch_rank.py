f175b7722cf62f22b51836a6e9b3b8e6a57fd815
arslan <maa8g09@soton.ac.uk>
2016-03-17 11:30:11 +0000

    convert ranked files from ff to h5

--- batch_rank.py
+++ batch_rank.py
@@ -4,8 +4,8 @@ import os
 
 direc = "/home/arslan/Desktop/ati-modes-copy/modes/A/kz2/ff_files"
 os.chdir(direc)
-command = "rankFiles.py -r 2"
-os.system(command)
+#command = "rankFiles.py -r 2"
+#os.system(command)
 command = "rankFiles.py -r 4"
 os.system(command)
 
---
+++ convert2H5-2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Convert all files in directory given from .ff to .h5")
+parser.add_argument("-d",
+                    "--Directory",
+                    metavar='\b',
+                    help="Directory where all rank folders are kept.",
+                    required=True)
+
+args = parser.parse_args()
+
+pwd = args.Directory
+
+#### List all directories in directory passed in...
+dirs = [name for name in os.listdir(pwd) if os.path.isdir(os.path.join(pwd, name))]
+
+
+#### Loop through all directories
+for directory in dirs:
+    #### Change into directory
+    os.chdir(directory)
+    print("\nChanged to", directory)
+    #### List files in directory
+    files = [fi for fi in os.listdir(directory) if os.path.isfile(os.path.join(directory,fi))]
+    print('\nConverting...')
+    for file in files:
+        file = str(file)
+        #### If the file is a channelflow binary...
+        if file[-3:] == ".ff":
+            file_ff = file
+            file_h5 = file[:-3] + ".h5"
+            ### Convert it
+            command = "\nfieldconvert " + file_ff + " ../" + file_h5
+            print(command)
+            os.system(command)
+
