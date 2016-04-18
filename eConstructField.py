15c80a6a61fb6713ea85a0f5d5fff27bc5f3456d
arslan <maa8g09@soton.ac.uk>
2016-04-18 15:29:17 +0100

    Write original attrs to disk from original H5 file, then read them correctly to create approximated field when reconstructing from modes

--- eConstructField.py
+++ eConstructField.py
@@ -110,6 +110,7 @@ ff_approximated.add_wall_boundaries()
 #================================================================
 #### Write approximated HDF5 file
 #================================================================
-fileName = args.File[:-3] + "_rank_" + str(rank) + ".h5"
+prefix = args.File.split("deconstructed")[0]
+fileName = prefix + "rank_" + str(rank) + ".h5"
 ut.write_H5(ff_approximated, deconstructed_field['original_attrs'],fileName) # need to write attributes to file...
 print("\n\nFinished\n")
