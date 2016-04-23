bfa11bfef4e2c7ce073282b2e40e15eb4b8ae898
arslan <maa8g09@soton.ac.uk>
2016-04-23 22:47:37 +0100

    Modified batch file to plot spatial average in x

--- bin/batch_NKH_approximations.py
+++ bin/batch_NKH_approximations.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import sys
+sys.path.append("/".join(sys.path[0].split("/")[:-1]))
 import Utils as ut
 parser = argparse.ArgumentParser(description="Run nkh searches")
 
@@ -27,43 +29,44 @@ directory = ut.format_Directory_Path(directory)
 os.chdir(directory)
 print(directory)
 #### Plot flow field
-plot = "ePlotField.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 0"
+plot = "e_Plot_Field.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 0 -a -l 16"
 print(plot)
 os.system(plot)
-plot = "ePlotField.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 2"
-print(plot)
-os.system(plot)
-#### ---NKH Search
-nkh = "findsoln -eqb -sn -log nkh-"+name+".log "+name+".h5"
-print(nkh)
-os.system(nkh)
-#### Deconstruct
-deconstruct = "eDeconstructField.py -d "+args.Details+" -f "+name+".h5"
-print(deconstruct)
-os.system(deconstruct)
+#plot = "ePlotField.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 2"
+#print(plot)
+#os.system(plot)
+##### ---NKH Search
+#nkh = "findsoln -eqb -sn -log nkh-"+name+".log "+name+".h5"
+#print(nkh)
+#os.system(nkh)
+##### Deconstruct
+#deconstruct = "eDeconstructField.py -d "+args.Details+" -f "+name+".h5"
+#print(deconstruct)
+#os.system(deconstruct)
 
 #### Rank: Full
 #### ---Construct
 print("\n======================================================================")
 print("RANK: Full")
 print("======================================================================")
-rankfull = "\n\neConstructField.py -f "+name+"_deconstructed.h5 -r 1000"
-print(rankfull)
-os.system(rankfull)
+#rankfull = "\n\neConstructField.py -f "+name+"_deconstructed.h5 -r 1000"
+#print(rankfull)
+#os.system(rankfull)
 fileName = name+"_rank_full"
 rankfull_d = directory + fileName
 os.chdir(rankfull_d)
 #### ---Plot flow field
-plot = "ePlotField.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0"
+#plot = "ePlotField.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0"
+plot = "e_Plot_Field.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 0 -a -l 16"
 print(plot)
 os.system(plot)
-plot = "ePlotField.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 2"
-print(plot)
-os.system(plot)
-#### ---NKH Search
-nkh = "findsoln -eqb -sn -log nkh-"+fileName+".log "+fileName+".h5"
-print(nkh)
-os.system(nkh)
+#plot = "ePlotField.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 2"
+#print(plot)
+#os.system(plot)
+##### ---NKH Search
+#nkh = "findsoln -eqb -sn -log nkh-"+fileName+".log "+fileName+".h5"
+#print(nkh)
+#os.system(nkh)
 
 #### Loop through ranks
 ranks = [2,4,10,20,40,60]
@@ -75,22 +78,23 @@ for r in range(0, len(ranks)):
     #### Change into main folder
     os.chdir(directory)
     #### Construct field into rank folder
-    construct = "\neConstructField.py -f "+name+"_deconstructed.h5 -r " + str(rank)
-    print(construct)
-    os.system(construct)
+#    construct = "\neConstructField.py -f "+name+"_deconstructed.h5 -r " + str(rank)
+#    print(construct)
+#    os.system(construct)
     #### Change into rank folder
     fileName = name+"_rank_" + str(rank)
     rank_dir = directory + fileName
     os.chdir(rank_dir)
     #### Plot flow field
-    plot = "\nePlotField.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0"
+#    plot = "e_Plot_Field.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0"
+    plot = "e_Plot_Field.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 0 -a -l 16"
     print(plot)
     os.system(plot)
-    plot = "\nePlotField.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 2"
-    print(plot)
-    os.system(plot)
-    #### NKH Search
-    nkh = "\nfindsoln -eqb -sn -log nkh-"+fileName+".log "+fileName+".h5"
-    print(nkh)
-    os.system(nkh)
-
+#    plot = "\nePlotField.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 2"
+#    print(plot)
+#    os.system(plot)
+#    #### NKH Search
+#    nkh = "\nfindsoln -eqb -sn -log nkh-"+fileName+".log "+fileName+".h5"
+#    print(nkh)
+#    os.system(nkh)
+#
