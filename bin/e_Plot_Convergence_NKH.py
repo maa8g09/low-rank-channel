127196ba74fadcae9c67f0d1eb9fced9363103d9
arslan <maa8g09@soton.ac.uk>
2016-04-23 16:40:46 +0100

    Changed L2Norm(du) plotting

--- bin/e_Plot_Convergence_NKH.py
+++ bin/e_Plot_Convergence_NKH.py
@@ -33,20 +33,20 @@ if args.Directory: # if a directory is given...
     for root, sub_dirs, files in os.walk(directory):
         for file in files:
             if file.endswith("convergence.asc"):
-                 print(os.path.join(root, file))
+#                 print(os.path.join(root, file))
                  # read in convergence file
                  # initialise convergence dictionary
                  all_convergence_data[fileName] = {}
                  # populate the dictionary
                  all_convergence_data[fileName] = ut.read_Convergence_NKH(file)
-        print("")        
+#        print("")
         for sub_dir in sub_dirs:
             sub_fileName = sub_dir
             sub_dir = os.path.join(root, sub_dir)
             os.chdir(sub_dir)
             for file in os.listdir(sub_dir):
                 if file.endswith("convergence.asc"):
-                    print(os.path.join(sub_dir, file))
+#                    print(os.path.join(sub_dir, file))
                     # read in convergence file
                     # initialise convergence dictionary
                     all_convergence_data[sub_fileName] = {}
@@ -54,6 +54,9 @@ if args.Directory: # if a directory is given...
                     all_convergence_data[sub_fileName] = ut.read_Convergence_NKH(file)
         break
     os.chdir(directory)
+    
+    ut.make_Folder(directory, "convergence_NKH", False):    
+    
     ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "L2Norm(u)")
     ut.plot_Convergence_NKH_multi(all_convergence_data, "ftotal", "L2Norm(u)")
     ut.plot_Convergence_NKH_multi(all_convergence_data, "fnewt", "L2Norm(u)")
@@ -64,9 +67,8 @@ if args.Directory: # if a directory is given...
     ut.plot_Convergence_NKH_multi(all_convergence_data, "fnewt", "L2Norm(G)")
     ut.plot_Convergence_NKH_multi(all_convergence_data, "fhook", "L2Norm(G)")
     
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "L2Norm(du)")
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "ftotal", "L2Norm(du)")
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "fnewt", "L2Norm(du)")
     ut.plot_Convergence_NKH_multi(all_convergence_data, "fhook", "L2Norm(du)")
     
     ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "GMRESerr")
+    
+    ut.print_EndMessage()
