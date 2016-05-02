e92ab55ba4195546baebed276de22f68fa1c6a27
arslan <maa8g09@soton.ac.uk>
2016-05-02 23:14:26 +0100

    Changed name of output directory

--- bin/e_Plot_Convergence_NKH.py
+++ bin/e_Plot_Convergence_NKH.py
@@ -21,52 +21,56 @@ args = parser.parse_args()
 if args.Directory: # if a directory is given...
     directory = ut.format_Directory_Path(args.Directory)
     os.chdir(directory)
-    # the last directory name is the case name.
-    fileName = directory.split("/")
-    fileName = fileName[-2]
-    # Read convergence files in this directory and all sub directories...
-    all_convergence_data = {}
-    for root, sub_dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith("convergence.asc"):
-#                 print(os.path.join(root, file))
-                 # read in convergence file
-                 # initialise convergence dictionary
-                 all_convergence_data[fileName] = {}
-                 # populate the dictionary
-                 all_convergence_data[fileName] = ut.read_Convergence_NKH(file)
-#        print("")
-        for sub_dir in sub_dirs:
-            sub_fileName = sub_dir
-            print(sub_dir)
-            sub_dir = os.path.join(root, sub_dir)
-            os.chdir(sub_dir)
-            for file in os.listdir(sub_dir):
-                if file.endswith("convergence.asc"):
-#                    print(os.path.join(sub_dir, file))
-                    # read in convergence file
-                    # initialise convergence dictionary
-                    all_convergence_data[sub_fileName] = {}
-                    # populate the dictionary
-                    all_convergence_data[sub_fileName] = ut.read_Convergence_NKH(file)
-        break
+else:
+    directory = os.getcwd()
+    directory = ut.format_Directory_Path(directory)
     os.chdir(directory)
-    
-    convergence_folder = ut.make_Folder(directory, "convergence_NKH", False)  
-    os.chdir(convergence_folder)
+# the last directory name is the case name.
+fileName = directory.split("/")
+fileName = fileName[-2]
+# Read convergence files in this directory and all sub directories...
+all_convergence_data = {}
+for root, sub_dirs, files in os.walk(directory):
+    for file in files:
+        if file.endswith("convergence.asc"):
+#                 print(os.path.join(root, file))
+             # read in convergence file
+             # initialise convergence dictionary
+             all_convergence_data[fileName] = {}
+             # populate the dictionary
+             all_convergence_data[fileName] = ut.read_Convergence_NKH(file)
+#        print("")
+    for sub_dir in sub_dirs:
+        sub_fileName = sub_dir
+        print(sub_dir)
+        sub_dir = os.path.join(root, sub_dir)
+        os.chdir(sub_dir)
+        for file in os.listdir(sub_dir):
+            if file.endswith("convergence.asc"):
+#                    print(os.path.join(sub_dir, file))
+                # read in convergence file
+                # initialise convergence dictionary
+                all_convergence_data[sub_fileName] = {}
+                # populate the dictionary
+                all_convergence_data[sub_fileName] = ut.read_Convergence_NKH(file)
+    break
+os.chdir(directory)
 
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "L2Norm(u)")
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "ftotal", "L2Norm(u)")
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "fnewt", "L2Norm(u)")
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "fhook", "L2Norm(u)")
-    
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "L2Norm(G)")
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "ftotal", "L2Norm(G)")
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "fnewt", "L2Norm(G)")
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "fhook", "L2Norm(G)")
-    
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "fhook", "L2Norm(du)")
-    
-    ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "GMRESerr")
-    
-    ut.print_EndMessage()
+convergence_folder = ut.make_Folder(directory, fileName+"_convergence_NKH", False)  
+os.chdir(convergence_folder)
+
+ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "L2Norm(u)")
+#ut.plot_Convergence_NKH_multi(all_convergence_data, "ftotal", "L2Norm(u)")
+#ut.plot_Convergence_NKH_multi(all_convergence_data, "fnewt", "L2Norm(u)")
+#ut.plot_Convergence_NKH_multi(all_convergence_data, "fhook", "L2Norm(u)")
+
+ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "L2Norm(G)")
+#ut.plot_Convergence_NKH_multi(all_convergence_data, "ftotal", "L2Norm(G)")
+#ut.plot_Convergence_NKH_multi(all_convergence_data, "fnewt", "L2Norm(G)")
+#ut.plot_Convergence_NKH_multi(all_convergence_data, "fhook", "L2Norm(G)")
+
+ut.plot_Convergence_NKH_multi(all_convergence_data, "fhook", "L2Norm(du)")
+
+ut.plot_Convergence_NKH_multi(all_convergence_data, "Newton_Steps", "GMRESerr")
+
+ut.print_EndMessage()
