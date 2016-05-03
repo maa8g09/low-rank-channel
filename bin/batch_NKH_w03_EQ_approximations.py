5f5bf3c04b7995c7160b1b320b34ca99ded9da05
arslan <maa8g09@soton.ac.uk>
2016-05-03 01:14:02 +0100

    Altered file to only output averaged plot

--- bin/batch_NKH_w03_EQ_approximations.py
+++ bin/batch_NKH_w03_EQ_approximations.py
@@ -32,31 +32,31 @@ print(directory)
 plot = "e_Plot_Field.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 0 -a -l 16"
 print(plot)
 os.system(plot)
-plot = "e_Plot_Field.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 0 -full -l 16"
-print(plot)
-os.system(plot)
+#plot = "e_Plot_Field.py -f "+name+".h5 -d "+args.Details+" -i 0 -n 0 -full -l 16"
+#print(plot)
+#os.system(plot)
 reynolds = 400
 if name == "eq6" or name == "eq12" or name == "eq13" or name == "eq14" or name == "eq15-1" or name == "eq15-2":
     reynolds = 330
 elif name == "eq8" or name == "eq16" or name == "eq17":
     reynolds = 270
 #### ---NKH Search
-nkh = "findsoln -eqb -R "+str(reynolds)+" -Nn 40 -sn -log nkh-"+name+".log "+name+".h5"
-print(nkh)
-os.system(nkh)
+#nkh = "findsoln -eqb -R "+str(reynolds)+" -Nn 40 -sn -log nkh-"+name+".log "+name+".h5"
+#print(nkh)
+#os.system(nkh)
 #### Deconstruct
-deconstruct = "e_Deconstruct_Field.py -d "+args.Details+" -f "+name+".h5"
-print(deconstruct)
-os.system(deconstruct)
+#deconstruct = "e_Deconstruct_Field.py -d "+args.Details+" -f "+name+".h5"
+#print(deconstruct)
+#os.system(deconstruct)
 
 #### Rank: Full
 #### ---Construct
 print("\n======================================================================")
 print("RANK: Full")
 print("======================================================================")
-rankfull = "\n\ne_Construct_Field.py -f "+name+"_deconstructed.h5 -r 1000"
-print(rankfull)
-os.system(rankfull)
+#rankfull = "\n\ne_Construct_Field.py -f "+name+"_deconstructed.h5 -r 1000"
+#print(rankfull)
+#os.system(rankfull)
 fileName = name+"_rank_full"
 rankfull_d = directory + fileName
 os.chdir(rankfull_d)
@@ -64,13 +64,13 @@ os.chdir(rankfull_d)
 plot = "e_Plot_Field.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0 -a -l 16"
 print(plot)
 os.system(plot)
-plot = "e_Plot_Field.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0 -full -l 16"
-print(plot)
-os.system(plot)
+#plot = "e_Plot_Field.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0 -full -l 16"
+#print(plot)
+#os.system(plot)
 #### ---NKH Search
-nkh = "findsoln -eqb -R "+str(reynolds)+" -Nn 40 -sn -log nkh-"+fileName+".log "+fileName+".h5"
-print(nkh)
-os.system(nkh)
+#nkh = "findsoln -eqb -R "+str(reynolds)+" -Nn 40 -sn -log nkh-"+fileName+".log "+fileName+".h5"
+#print(nkh)
+#os.system(nkh)
 
 #### Loop through ranks
 ranks = [2,4,10,20,40,60]
@@ -82,9 +82,9 @@ for r in range(0, len(ranks)):
     #### Change into main folder
     os.chdir(directory)
     #### Construct field into rank folder
-    construct = "\ne_Construct_Field.py -f "+name+"_deconstructed.h5 -r " + str(rank)
-    print(construct)
-    os.system(construct)
+#    construct = "\ne_Construct_Field.py -f "+name+"_deconstructed.h5 -r " + str(rank)
+#    print(construct)
+#    os.system(construct)
     #### Change into rank folder
     fileName = name+"_rank_" + str(rank).zfill(2)
     rank_dir = directory + fileName
@@ -93,11 +93,11 @@ for r in range(0, len(ranks)):
     plot = "e_Plot_Field.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0 -a -l 16"
     print(plot)
     os.system(plot)
-    plot = "e_Plot_Field.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0 -full -l 16"
-    print(plot)
-    os.system(plot)
+#    plot = "e_Plot_Field.py -f "+fileName+".h5 -d ../"+args.Details+" -i 0 -n 0 -full -l 16"
+#    print(plot)
+#    os.system(plot)
     #### NKH Search
-    nkh = "\nfindsoln -eqb -R "+str(reynolds)+" -Nn 40 -sn -log nkh-"+fileName+".log "+fileName+".h5"
-    print(nkh)
-    os.system(nkh)
+#    nkh = "\nfindsoln -eqb -R "+str(reynolds)+" -Nn 40 -sn -log nkh-"+fileName+".log "+fileName+".h5"
+#    print(nkh)
+#    os.system(nkh)
 
